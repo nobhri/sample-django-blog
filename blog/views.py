b18fececee23e1d6e6d25ac704ca2514a7c88b1f
nobhri <nob.third.stone.from.the.sun@gmail.com>
2023-02-10 18:44:31 +0900

    feat: add 'about' page

--- blog/views.py
+++ blog/views.py
@@ -9,3 +9,6 @@ def post_list(request):
 def post_detail(request,pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+def about(request):
+    return render(request, 'blog/about.html')
